46171f6bc1665505799b3c363a8fddfe6e077924
Allan LeSage <allan.lesage@gmail.com>
2014-02-03 21:39:46 -0600

    Slavishly correct PEP8 and pyflakes issues; also indent and establish main().

--- scripts/download.py
+++ scripts/download.py
@@ -9,13 +9,6 @@ import getopt
 import sys
 
 
-config = ConfigParser.ConfigParser()
-config.readfp(open('config.ini'))
-
-options = {}
-options['verbose'] = config.get('debug', 'verbose')
-
-
 class Fetcher(threading.Thread):
     def __init__(self, queue, path, options):
         threading.Thread.__init__(self)
@@ -23,7 +16,7 @@ class Fetcher(threading.Thread):
         self.path = path
         self.options = options
 
-    def run(self):
+    def run(self, verbose=False):
         while 1:
             try:
                 url = self.queue.get_nowait()
@@ -32,7 +25,7 @@ class Fetcher(threading.Thread):
 
             filename = os.path.basename(url)
 
-            if(options['verbose']):
+            if(verbose):
                 print "fetching " + filename
 
             f = "%s/%s" % (self.path, filename)
@@ -41,51 +34,65 @@ class Fetcher(threading.Thread):
             if (zipfile.is_zipfile(f)):
                 zip = zipfile.ZipFile(f, "r")
                 zip.extractall(self.path)
-                if(options['verbose']):
+                if(verbose):
                     print "extracting " + filename
 
             os.remove(f)
 
-path = config.get('download', 'directory')
-absolute_path = os.path.abspath(path)
-try:
-    os.chdir(absolute_path)
-except OSError:
-    print "Directory %s does not exist, creating..." % absolute_path
-    os.makedirs(absolute_path)
 
-YEAR = False
+def main():
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "y:", ["year="])
-except getopt.GetoptError as e:
-    print 'invalid arguments'
-    raise SystemExit
+    config = ConfigParser.ConfigParser()
+    config.readfp(open('config.ini'))
 
-for o, a in opts:
-    if o in ('-y', '--year'): YEAR = a
+    options = {}
+    options['verbose'] = config.get('debug', 'verbose')
 
-if YEAR:
-    print "fetching retrosheet files for year %s..." % YEAR
-else:
-    print "fetching retrosheet files..."
+    path = config.get('download', 'directory')
+    absolute_path = os.path.abspath(path)
+    try:
+        os.chdir(absolute_path)
+    except OSError:
+        print "Directory %s does not exist, creating..." % absolute_path
+        os.makedirs(absolute_path)
 
-queue = Queue.Queue()
-retrosheet_url = config.get('retrosheet', 'url')
-pattern = r'(\d{4}?)eve\.zip'
-for match in re.finditer(pattern, urllib.urlopen(retrosheet_url).read(), re.S):
-    if YEAR and match.group(1) != YEAR: continue
-    url = 'http://www.retrosheet.org/events/%seve.zip' % match.group(1)
-    queue.put(url)
+    YEAR = False
 
-threads = []
-num_threads = config.getint('download', 'num_threads')
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "y:", ["year="])
+    except getopt.GetoptError:
+        print 'invalid arguments'
+        raise SystemExit
 
-for i in range(num_threads):
-    t = Fetcher(queue, absolute_path, options)
-    t.start()
-    threads.append(t)
+    for o, a in opts:
+        if o in ('-y', '--year'): YEAR = a
 
-# finish fetching before processing events into CSV
-for thread in threads:
-    thread.join()
+    if YEAR:
+        print "fetching retrosheet files for year %s..." % YEAR
+    else:
+        print "fetching retrosheet files..."
+
+    queue = Queue.Queue()
+    retrosheet_url = config.get('retrosheet', 'url')
+    pattern = r'(\d{4}?)eve\.zip'
+    for match in re.finditer(pattern,
+                             urllib.urlopen(retrosheet_url).read(), re.S):
+        if YEAR and match.group(1) != YEAR: continue
+        url = 'http://www.retrosheet.org/events/%seve.zip' % match.group(1)
+        queue.put(url)
+
+    threads = []
+    num_threads = config.getint('download', 'num_threads')
+
+    for i in range(num_threads):
+        t = Fetcher(queue, absolute_path, options)
+        t.start()
+        threads.append(t)
+
+    # finish fetching before processing events into CSV
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == '__main__':
+    main()
